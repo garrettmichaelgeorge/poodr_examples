a2a8b57fa0e1712953cef14e7e5c4dda97c6e4bb
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-01-11 12:46:44 -0500

    Listing 2.19 Add passing test for Wheel#diameter

--- test/2/wheel_test.rb
+++ test/2/wheel_test.rb
@@ -4,6 +4,12 @@ require_relative '../../2/wheel'
 describe Wheel do
   subject { Wheel.new(26, 1.5) }
 
+  describe '#diameter' do
+    it 'calculates the diameter' do
+      _(subject.diameter).must_be_within_epsilon 29
+    end
+  end
+
   describe '#circumference' do
     it 'calculates the circumferences' do
       _(subject.circumference).must_be_within_epsilon 91.11
