c0b2e3cd7ef4245e30449139a1eac04a1834234d
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-01-11 12:09:19 -0500

    Listing 2.15 Split References#diameters into separate methods for iteration and calculation

--- 2/references.rb
+++ 2/references.rb
@@ -5,10 +5,14 @@ class References
     @wheels = wheelify(data)
   end
 
+  # first - iterate over the array
   def diameters
-    wheels.collect do |wheel|
-      wheel.rim + (wheel.tire * 2)
-    end
+    wheels.collect { |wheel| diameter(wheel) }
+  end
+
+  # second - calculate diameter of ONE wheel
+  def diameter(wheel)
+    wheel.rim + (wheel.tire * 2)
   end
 
   Wheel = Struct.new(:rim, :tire)
